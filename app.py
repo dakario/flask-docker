e17c68523653b50ff05bc350dc5f05fdfedf062d
Aliou <alioubafr@gmail.com>
2018-06-18 13:38:23 +0200

    flask with docker Hostname

---
+++ app.py
@@ -0,0 +1,10 @@
+import socket
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello Dakar Inno Day. "+ socket.gethostname()
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')

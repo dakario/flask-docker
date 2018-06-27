0f48be5ffa68928910177c8bdc264f3fff25d369
Aliou BA <alioubafr@gmail.com>
2018-06-27 07:50:16 +0000

    Port 80

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Dakar Inno Day. "+ socket.gethostname()
+    return "Hello Dakar Innovation Day. "+ socket.gethostname()
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',, port=80)
